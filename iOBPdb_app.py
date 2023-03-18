8562bd79b0488b3638e4e0efef147d6e639c1335
sshuklz <58276851+sshuklz@users.noreply.github.com>
2023-03-17 22:00:27 -0700

    Add files via upload

--- iOBPdb_app.py
+++ iOBPdb_app.py
@@ -171,14 +171,14 @@ page_0_layout = html.Div( children = [
         
         children=[
             
-            html.Img(src="/assets/BAS copy.gif", width = 250,height = 250),
+            html.Img(src="/assets/BAS_lr.png", width = 250,height = 250),
             
             dmc.Text(
                 "iOBPdb Home",
                 style={"fontSize": 60},
             ),
             
-            html.Img(src="/assets/cartoon copy.gif", width = 250,height = 250)
+            html.Img(src="/assets/cartoon_lr.png", width = 250,height = 250)
             
         ], style={"marginLeft": 120,"marginBottom":25}
     ),
